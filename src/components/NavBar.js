0cdd0acd8508070ed353fc69dafc7445f68156eb
nadpalu <nadia_lucadei@outlook.com>
2022-06-02 01:29:29 -0300

    desafio menu e-commerce

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+function Header () {
+	return (<header>
+				<h2>"KPOP STORE"</h2>
+				<nav>
+					<a href="#" className="paddingLinks">Home</a>
+					<a href="#" className="paddingLinks">Productos</a>
+					<a href="#" className="paddingLinks">Grupos</a>
+					<a href="#" className="paddingLinks">Noticias</a>
+					<a href="#" className="paddingLinks">Contacto</a>
+				</nav>
+			</header>
+	);
+};
+
+export default Header;
